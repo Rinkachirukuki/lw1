6d2ed0d4ba13724b84d019e45be6b25997c7bbb7
Rinkachirukuki <belkossquirrel@gmail.com>
2021-04-29 21:53:43 +0500

    FlyWay scripts

--- src/main/java/com/SoftwareDevelopment/TrComp/controllers/AchievementController.java
+++ src/main/java/com/SoftwareDevelopment/TrComp/controllers/AchievementController.java
@@ -1,6 +1,8 @@
 package com.SoftwareDevelopment.TrComp.controllers;
 
+import com.SoftwareDevelopment.TrComp.models.Achievement;
 import com.SoftwareDevelopment.TrComp.models.Customer;
+import com.SoftwareDevelopment.TrComp.models.Driver;
 import com.SoftwareDevelopment.TrComp.services.AchievementService;
 import com.SoftwareDevelopment.TrComp.services.CustomerService;
 import com.SoftwareDevelopment.TrComp.services.GenderService;
@@ -30,13 +32,17 @@ public class AchievementController {
 
 
     @GetMapping("/list")
-    public String customerAchievementList(Pageable page, @RequestParam int customerId, Model model) {
-
-        model.addAttribute("customer", customerService.findById(customerId));
+    public String customerAchievementList(Pageable page, @RequestParam(value= "customerId",defaultValue = "0", required=false) int customerId, Model model) {
 
         model.addAttribute("achievementList", achievementService.findAll(page));
 
-        return "achievement/show-all-achievements";
+        if (customerId != 0) {
+            model.addAttribute("customer", customerService.findById(customerId));
+            return "achievement/show-all-achievements-by-customer";
+        }
+        else{
+            return "achievement/show-all-achievements";
+        }
     }
 
     @GetMapping("/customerList")
@@ -45,6 +51,28 @@ public class AchievementController {
         model.addAttribute("achievement", achievementService.findById(achievementId));
 
         return "achievement/show-all-customers-with-achievement";
+    }
+
+    @GetMapping("/add")
+    public String addAchievement(Pageable page, Model model) {
+
+        model.addAttribute("achievement", new Achievement());
+
+        return "achievement/update-form";
+    }
+
+    @PostMapping("/save")
+    public String achievementSave(@ModelAttribute("vehicle") Achievement achievement) {
+        achievementService.save(achievement);
+        return "redirect:/achievement/list";
+    }
+
+    @GetMapping("/delete")
+    public String achievementDelete(@RequestParam("id") int id) {
+
+        achievementService.deleteById(id);
+
+        return "redirect:/achievement/list";
     }
 
 }
---
+++ src/main/resources/db/migration/V1__Init_DB.sql
@@ -0,0 +1,205 @@
+-- MySQL dump 10.13  Distrib 8.0.23, for Win64 (x86_64)
+--
+-- Host: localhost    Database: usersdb
+-- ------------------------------------------------------
+-- Server version	8.0.23
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `achievement`
+--
+
+DROP TABLE IF EXISTS `achievement`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `achievement` (
+  `achievement_id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`achievement_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `customer`
+--
+
+DROP TABLE IF EXISTS `customer`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `customer` (
+  `customer_id` int NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(255) DEFAULT NULL,
+  `last_name` varchar(255) DEFAULT NULL,
+  `patronymic` varchar(255) DEFAULT NULL,
+  `gender_name` varchar(255) DEFAULT NULL,
+  `privilege_name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`customer_id`),
+  KEY `FK7so4kdsl3n3kcjdu61obawsyd` (`gender_name`),
+  KEY `FKpgrqyoerwqcna8skjq7o42ktg` (`privilege_name`),
+  CONSTRAINT `FK7so4kdsl3n3kcjdu61obawsyd` FOREIGN KEY (`gender_name`) REFERENCES `gender` (`gender_name`),
+  CONSTRAINT `FKpgrqyoerwqcna8skjq7o42ktg` FOREIGN KEY (`privilege_name`) REFERENCES `privilege` (`privilege_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `customer_achievement`
+--
+
+DROP TABLE IF EXISTS `customer_achievement`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `customer_achievement` (
+  `customer_fk` int NOT NULL,
+  `achievement_fk` int NOT NULL,
+  PRIMARY KEY (`customer_fk`,`achievement_fk`),
+  KEY `FKgupx23gi2djhdb47bgat9amb2` (`achievement_fk`),
+  CONSTRAINT `FK64av7vnahv1ghwv64uobov3ec` FOREIGN KEY (`customer_fk`) REFERENCES `customer` (`customer_id`),
+  CONSTRAINT `FKgupx23gi2djhdb47bgat9amb2` FOREIGN KEY (`achievement_fk`) REFERENCES `achievement` (`achievement_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `driver`
+--
+
+DROP TABLE IF EXISTS `driver`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `driver` (
+  `driver_id` int NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(255) DEFAULT NULL,
+  `last_name` varchar(255) DEFAULT NULL,
+  `patronymic` varchar(255) DEFAULT NULL,
+  `middle_name` varchar(255) DEFAULT NULL,
+  `gender_name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`driver_id`),
+  KEY `FKj96csr3ri0delest2av8n00dn` (`gender_name`),
+  CONSTRAINT `FKj96csr3ri0delest2av8n00dn` FOREIGN KEY (`gender_name`) REFERENCES `gender` (`gender_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `gender`
+--
+
+DROP TABLE IF EXISTS `gender`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `gender` (
+  `gender_name` varchar(255) NOT NULL,
+  PRIMARY KEY (`gender_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `mark`
+--
+
+DROP TABLE IF EXISTS `mark`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `mark` (
+  `mark_name` varchar(255) NOT NULL,
+  PRIMARY KEY (`mark_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `privilege`
+--
+
+DROP TABLE IF EXISTS `privilege`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `privilege` (
+  `privilege_name` varchar(255) NOT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `discount_size` int DEFAULT NULL,
+  PRIMARY KEY (`privilege_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `review`
+--
+
+DROP TABLE IF EXISTS `review`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `review` (
+  `review_id` int NOT NULL AUTO_INCREMENT,
+  `description` varchar(255) DEFAULT NULL,
+  `grade` int DEFAULT NULL,
+  `customer_id` int DEFAULT NULL,
+  `driver_id` int DEFAULT NULL,
+  PRIMARY KEY (`review_id`),
+  KEY `FKgce54o0p6uugoc2tev4awewly` (`customer_id`),
+  KEY `FKc5ins2nksp4wrkoiusu9quc3g` (`driver_id`),
+  CONSTRAINT `FKc5ins2nksp4wrkoiusu9quc3g` FOREIGN KEY (`driver_id`) REFERENCES `driver` (`driver_id`),
+  CONSTRAINT `FKgce54o0p6uugoc2tev4awewly` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`customer_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `taxi_order`
+--
+
+DROP TABLE IF EXISTS `taxi_order`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `taxi_order` (
+  `order_id` int NOT NULL AUTO_INCREMENT,
+  `destination_point` varchar(255) DEFAULT NULL,
+  `price` int DEFAULT NULL,
+  `start_point` varchar(255) DEFAULT NULL,
+  `customer_id` int DEFAULT NULL,
+  `vehicle_id` int DEFAULT NULL,
+  PRIMARY KEY (`order_id`),
+  KEY `FKiy7t3e104f5shmybgom88rowe` (`customer_id`),
+  KEY `FK9ijxng0fdl7ts2rofoiph62wi` (`vehicle_id`),
+  CONSTRAINT `FK9ijxng0fdl7ts2rofoiph62wi` FOREIGN KEY (`vehicle_id`) REFERENCES `vehicle` (`vehicle_id`),
+  CONSTRAINT `FKiy7t3e104f5shmybgom88rowe` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`customer_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `vehicle`
+--
+
+DROP TABLE IF EXISTS `vehicle`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `vehicle` (
+  `vehicle_id` int NOT NULL AUTO_INCREMENT,
+  `model` varchar(255) DEFAULT NULL,
+  `number` varchar(255) DEFAULT NULL,
+  `driver_id` int DEFAULT NULL,
+  `mark_name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`vehicle_id`),
+  KEY `FKdpor9ohov2f3optwe7twe49tt` (`driver_id`),
+  KEY `FKt3jve3vof0wk911cj7v849stj` (`mark_name`),
+  CONSTRAINT `FKdpor9ohov2f3optwe7twe49tt` FOREIGN KEY (`driver_id`) REFERENCES `driver` (`driver_id`),
+  CONSTRAINT `FKt3jve3vof0wk911cj7v849stj` FOREIGN KEY (`mark_name`) REFERENCES `mark` (`mark_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-04-29 20:47:49
